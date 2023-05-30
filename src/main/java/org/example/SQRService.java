3a2b6ed29b88bac89f2b994428e34e703ff3687b
Kolya <kolya.samarskiy.2015@mail.ru>
2023-05-30 17:12:24 +0300

    Initial commit

---
+++ src/main/java/org/example/SQRService.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class SQRService {
+
+    public int squareRoot(int min, int max) {
+        int result = 0;
+
+        for (int number = 10; number <= 99; number++) {
+            int sqRoot = number * number;
+            if (sqRoot >= min && sqRoot <= max) {
+                result++;
+            }
+        }
+        return result;
+    }
+}
---
+++ src/main/java/org/example/SQRServiceTest.java
@@ -0,0 +1,19 @@
+package org.example;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+public class SQRServiceTest{
+    @ParameterizedTest
+    @CsvSource({
+            "3,15,225",
+            "3,16,256",
+            "3,17,289"
+    })
+    public void theSquareRootInRange(int expected, int number, int sqRoot) {
+        SQRService service = new SQRService();
+
+        int actual = service.squareRoot(200, 300);
+        Assertions.assertEquals(expected, actual);
+    }
+}
